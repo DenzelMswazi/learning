e46ee01a1e91ca44a85bc83f06b7fe71d9934c9d
deeMswazi <denzelmwangi94@gmail.com>
2025-01-29 11:58:39 +0300

    update import to show the navbar

---
+++ client/src/components/NonDashboardNavbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NonDashboardNavbar = () => {
+  return (
+   <nav className='nondashboard-navbar'>
+     Hello
+   </nav>
+  )
+}
+
+export default NonDashboardNavbar
