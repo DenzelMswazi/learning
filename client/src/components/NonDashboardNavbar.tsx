e258e31ea2f40842842cf4aea93e8d2a52c9c2b6
deeMswazi <denzelmwangi94@gmail.com>
2025-02-03 16:20:19 +0300

    add import link to the navbar

--- client/src/components/NonDashboardNavbar.tsx
+++ client/src/components/NonDashboardNavbar.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'lucide-react'
 import React from 'react'
 
 const NonDashboardNavbar = () => {
   return (
-   <nav className='nondashboard-navbar'>
+   <nav className="nondashboard-navbar">
      <div className="nondashboard-navbar__container">
-      
+      <Link href="/" className="nondashboard-navbar__brand">
+        EDROH
+      </Link>
      </div>
    </nav>
   )
