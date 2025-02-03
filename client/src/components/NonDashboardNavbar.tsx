7d17153da8d725779320ca750e87e0f5ab633f7c
deeMswazi <denzelmwangi94@gmail.com>
2025-02-03 16:14:26 +0300

    add block element modifier

--- client/src/components/NonDashboardNavbar.tsx
+++ client/src/components/NonDashboardNavbar.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const NonDashboardNavbar = () => {
   return (
    <nav className='nondashboard-navbar'>
-     Hello
+     <div className="nondashboard-navbar__container">
+      
+     </div>
    </nav>
   )
 }
