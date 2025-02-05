d9ba983ae04452d3f40a21022d8d53f43b4dbe11
deeMswazi <denzelmwangi94@gmail.com>
2025-02-05 12:31:38 +0300

    add animation effect

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import React from 'react';
 import { motion } from "framer-motion"
 
 const Landing = () => {
   return (
     <motion.div
-    initial={{opacity: 0}}
+    initial={{ opacity: 0}}
+    animate={{ opacity: 1}}
+    transition={{ duration: 0.5}}
+    className="landing"
     >Landing</motion.div>
   )
 }
