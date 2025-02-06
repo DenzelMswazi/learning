7c1e37b61571216d580aa420d8d01ad9ebdba52b
deeMswazi <denzelmwangi94@gmail.com>
2025-02-06 10:34:25 +0300

    create the footer component

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -13,11 +13,30 @@ const LoadingSkeleton = () => {
       <div className="landing-skeleton__hero">
         <div className="landing-skeleton__hero-content">
           <Skeleton className="landing-skeleton__title" />
+          <Skeleton className="landing-skeleton__subtitle" />
+          <Skeleton className="landing-skeleton__subtitle-secondary" />
+          <Skeleton className="landing-skeleton__button" />
         </div>
+        <Skeleton className="landing-skeleton__hero-image" />
+      </div>
+      <div className="landing-skeleton__featured">
+      <Skeleton className="landing-skeleton__featured-title" />
+      <Skeleton className="landing-skeleton__featured-description" />
+
+      <div className="landing-skeleton__tags">
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((_, index) => (
+          <Skeleton key={index} className="landing-skeleton__tag" />
+        ))}
+      </div>
+      <div className="landing-skeleton__courses">
+      {[1, 2, 3, 4].map((_, index) => (
+          <Skeleton key={index} className="landing-skeleton__course-card" />
+        ))}
       </div>
     </div>
-  )
-}
+    </div>
+  );
+};
 
 const Landing = () => {
    const currentImage = useCarousel({ totalImages: 3}) 
